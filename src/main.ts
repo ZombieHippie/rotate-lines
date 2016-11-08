cce80deff834d0b4c51ba8eaebae60d997db539b
Cole R Lawrence <msgzht@gmail.com>
2016-11-08 16:09:17 -0600

    Do not format output with visual helpers

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ const rot = Rotator.fromString(contents, 11)
 
 rot.getRotations()
     .forEach((r, i) => {
-        let t = r.slice(0, 1).toFormatString()
+        let t = r.slice(0, 1).toString()
         writeFileSync(`out/n${i}t.txt`, t, 'utf8')
-        let fm = r.slice(1).toFormatString()
+        let fm = r.slice(1).toString()
         writeFileSync(`out/n${i}.txt`, fm, 'utf8')
     })
